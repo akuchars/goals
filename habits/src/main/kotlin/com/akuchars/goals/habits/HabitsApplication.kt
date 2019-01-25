93f82f978020ef5f7d2cedaf9909a780a396add5
Adam Kucharski <adam.kucharski@unity.pl>
2019-01-25 08:52:56 +0100

    Utworzenie projektu - dodanie vavra, querydsla, mockk - próba dodania liquibase

---
+++ habits/src/main/kotlin/com/akuchars/goals/habits/HabitsApplication.kt
@@ -0,0 +1,12 @@
+package com.akuchars.goals.habits
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabitsApplication
+
+fun main(args: Array<String>) {
+    runApplication<HabitsApplication>(*args)
+}
+
