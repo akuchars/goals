a3cdc26a7633e3f515e6e46f6f13e687ecec15cf
Adam Kucharski <adam.kucharski@unity.pl>
2019-01-30 07:47:06 +0100

    Poprawki pod circle ci

--- habits/src/main/kotlin/com/akuchars/goals/habits/application/common/command/GroupService.kt
+++ habits/src/main/kotlin/com/akuchars/goals/habits/application/common/command/GroupService.kt
@@ -2,10 +2,7 @@ package com.akuchars.goals.habits.application.common.command
 
 import com.akuchars.goals.habits.application.goal.query.TemplateQueryService
 import com.akuchars.goals.habits.domain.goal.model.Group
-import com.akuchars.goals.habits.domain.goal.model.QGroup
-import com.akuchars.goals.habits.domain.goal.model.QTemplate
 import com.akuchars.goals.habits.domain.goal.repository.GroupRepository
-import com.akuchars.goals.habits.domain.goal.repository.TemplateRepository
 import com.akuchars.goals.habits.kernel.saveWithWalidate
 import com.akuchars.goals.habits.rest.dto.goal.GroupRestDto
 import io.vavr.control.Either
@@ -18,9 +15,6 @@ class GroupService(
         val colorConverter: ColorConverter,
         val templateQueryService: TemplateQueryService
 ) {
-
-    private val template: QTemplate = QTemplate.template
-    private val group: QGroup = QGroup.group
 
     @Transactional
     fun save(groupRestDto: GroupRestDto): Either<Throwable, Group> {
